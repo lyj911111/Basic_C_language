1feb5ae342ffd11f67e438f73771cfe3d53a1d4f
Jay <lyj911111@naver.com>
2018-08-03 15:28:49 +0900

    strncpy 사용 방법 추가, 문자열 복사 안전하게 사용하는 방법 (추천하는  방식)

--- strcpy_strncpy_함수_예제.c
+++ strcpy_strncpy_함수_예제.c
@@ -14,7 +14,10 @@ int main()
 	char str1[30] = "simple string";	//	str1 의 string을 str2 으로 복사할 예정임.
 	char str2[30];
 
-	strcpy(str2, str1);
+	//strcpy(str2, str1);
+	strncpy(str2, str1, sizeof(str2));	//	str1에 저장된 문자열을 str2로 복사하되, str2의 최대 크기는 sizeof(str2)의 반환값 만큼으로 한정해라.
+										//	str2값의 크기보다 커지면, 메모리 침범이 이루어지기 때문에, 안정적인 사용 방법임!
+
 
 	printf("%s", str2);
 
