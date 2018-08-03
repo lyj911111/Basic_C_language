553818c3e266d3a25389916182a7d6d0c036652d
Jay <lyj911111@naver.com>
2018-08-03 14:58:01 +0900

    // 예제 추가. strlen() 함수 활용하기.

--- 문자열_길이_측정함수_strlen.c
+++ 문자열_길이_측정함수_strlen.c
@@ -7,7 +7,34 @@
 			  ex) strlen(str) 아래 의 경우, NULL 문자를 제외한 7을 반환함.
 */
 
+// 예제 작성.
 
+void RemoveBSN(char str[])
+{
+	int len = strlen(str);								//	len에는 'Enter키'포함, NULL문자 빼고, 7이 저장됨. [123456'\n']
+	str[len - 1] = 0;									//	str[7-1] = str[6] 자리에 0을 대입. 즉, 'Enter키' 자리에 0을 대입하여 
+}														//	문자열을 6에서 끝냄.
+
+int main()
+{
+	char str[100];										
+	printf("문자열 입력: ");
+	fgets(str,sizeof(str),stdin);						//	키보드로 부터 입력 받음 ,123456 를 입력하면
+	printf("길이: %d, 내용: %s \n", strlen(str), str);
+
+	RemoveBSN(str);										//	위에 함수 작동.
+
+	printf("길이: %d, 내용: %s \n", strlen(str), str);	//	함수 작동 전과 후 비교해보기.
+	return 0;
+}
+
+
+
+
+
+
+
+/*
 int main()
 {
 	char str[] = "1234567";
@@ -15,3 +42,4 @@ int main()
 									// 그러나 이 경우 %d 로 충분히 표현 가능.
 									// %u 와 %d 의 출력할 수 있는 값의 차이는 있지만, 큰 값이 아니므로 상관 없다.
 }
+*/
