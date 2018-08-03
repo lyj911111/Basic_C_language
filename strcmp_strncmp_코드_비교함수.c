f1f24790bb730eb9f30f5169569ac853bd0f1f17
Jay <lyj911111@naver.com>
2018-08-03 18:01:36 +0900

    문자열 비교함수 같은가? 다른가? 비교하여 int값 출력.

---
+++ strcmp_strncmp_코드_비교함수.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+// String Compare 문자열 비교 함수 (같은가? 다른가?)
+/*
+
+	함수 원형:
+	int strcmp(const * s1, const * s2);					왼쪽이크면 0보다 큼, 오른쪽이 크면 0보다 작음.
+	int strncmp(const * s1, const * s2, sizt_t n);
+
+	-> 두 문자열이 같으면 0을 반환, s1이 크면 0보다 큰값 반환, s2가 크면 0보다 작은값을 반환. 
+		값의 크고작음은 '아스키코드 값'에 근거함.
+		값의 크기는 알 수 없다. 0보다 크고 작음을 이용해야함.
+
+		A: 0x41		알파벳이 클수록 값이 큼.   a : 0x61 소문자가 대문자보다 값이 큼. (아스키 코드표 참고) 
+		B: 0x42								   b : 0x62
+		C: 0x43								   c : 0x63
+		D: 0x44								   d : 0x64
+
+		맨앞에 부터 비교하면서 문자열이 크면 이미 판명이 나버림.
+*/
+
+int main()
+{
+	printf("%d \n", strcmp("ABCD", "ABCD"));		//	같으므로 0을 반환
+
+	printf("%d \n", strcmp("ABCD", "ABCC"));		//	C < D 이므로 s1이 값이큼. 0보다 큰값 반환.				(왼쪽이 큼)
+	printf("%d \n", strcmp("ABCD", "ABCDE"));		//	ABCD까지 같으나, E값이 나와서 s2가 더 큼. 0보다 작은값 반환.(오른쪽이 큼)
+
+	return 0;
+}
