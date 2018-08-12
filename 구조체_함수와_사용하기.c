9edf624eea8eade23d96fa7ecb61a8751823ad43
Jay <lyj911111@naver.com>
2018-08-12 20:05:32 +0900

    구조체를 함수에 쓰는법.

---
+++ 구조체_함수와_사용하기.c
@@ -0,0 +1,7 @@
+﻿1>------ 빌드 시작: 프로젝트: Project3, 구성: Debug Win32 ------
+1>소스.c
+1>c:\users\lee won jae\desktop\c_language\project3\project3\소스.c(18): warning C4996: 'scanf': This function or variable may be unsafe. Consider using scanf_s instead. To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for details.
+1>c:\program files (x86)\windows kits\10\include\10.0.16299.0\ucrt\stdio.h(1272): note: 'scanf' 선언을 참조하십시오.
+1>Project3.vcxproj -> C:\Users\Lee Won Jae\Desktop\C_language\Project3\Debug\Project3.exe
+1>"Project3.vcxproj" 프로젝트를 빌드했습니다.
+========== 빌드: 성공 1, 실패 0, 최신 0, 생략 0 ==========
