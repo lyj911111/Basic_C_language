3afb73f62d099aaffd346b10ed04a7ee2b40e95d
Jay <lyj911111@naver.com>
2018-08-12 19:31:45 +0900

    typedef를 이용해 구조체를 간단히 선언하고 활용하는 예제 추가

--- Typedef선언_구조체간단히선언.c
+++ Typedef선언_구조체간단히선언.c
@@ -10,7 +10,7 @@
 */
 
 //선언 예제.
-
+/*
 typedef int INT;
 typedef int * PTR_INT;
 
@@ -47,6 +47,39 @@ int main()
 	struct point pos1;   // <= 이전에 선언했던 방식.
 
 	Point pos2;		//	<= typedef를 선언하고 이후 구조체를 선언하는 방식. (struct를 쓸 필요가 없다)
+
+	return 0;
+}*/
+
+// 구조체 정의와 typedef 선언 관련 예제.
+
+typedef struct point
+{
+	xpos;
+	ypos;
+}Point;
+
+typedef struct person
+{
+	char name[20];
+	char phoneNum[20];
+	int age;
+}Person;
+
+///////// 더 간단하게 선언할 수 있다.
+
+typedef struct //여기 생략할 수도 있다.
+{
+	char name[20];
+	char phoneNum[20];
+	int age;
+}Person;		// 어차피 Person을 주로 사용하니까.
+
+
+int main()
+{
+	Point pos = { 10 , 20 };		//	struct Point자료형선언 pos를 할당, 초기값 대입. { xpos = 10, ypos= 20 }이라는 뜻 
+	Person man = {"이승기", };
 
 	return 0;
 }
